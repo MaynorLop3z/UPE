4ed1ca23b49fe22b51ed7ec2803a3e6b531e0104
luis <luis@luis-Latitude-D610>
2015-08-16 01:32:06 -0600

    modificaciones medianoche 16 de agosto 2015

---
+++ application/models/Usuario_model.php
@@ -0,0 +1,29 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Usuario_model extends CI_Model {
+
+    public function construct() {
+        parent::__construct();
+        $this->load->database(); //con esto hacemos que pueda cargar nuestra base de datos con el modelo
+    }
+
+    public function login($username, $password) {
+        try {
+
+            $this->db->where('NombreUsuario', $username);
+            $this->db->where('ContraseniaUsuario', $password);
+            $q = $this->db->get('Usuarios');
+            if ($q->num_rows() > 0) {
+                return true;   
+            } else {
+                return false;
+            }
+        } catch (Exception $exc) {
+            echo $exc->getTraceAsString();
+        }
+    }
+
+}
