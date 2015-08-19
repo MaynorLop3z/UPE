e08bb63574e601284a4f9992a5b295233ac463f8
johanna <rodrjoha1.3@gmail.com>
2015-08-18 23:01:33 -0600

    Vista del Formulario agregar Usuario

--- application/views/usuarioNuevo.php
+++ application/views/usuarioNuevo.php
@@ -1,5 +1,39 @@
-<fieldset>
-    <legend>Pagina cargada con ajax </legend>
-    <br/>
-    <p>Pues bien, aqui les demuestro lo facil que es ajax, para cargar páginas en ciertas secciones de nuestro sitio web</p>
-</fieldset>
+<html>
+    <body>
+
+        <form action="" class="form-horizontal" method="post">
+            <fieldset>
+                <legend>Agregar Usuario:</legend> 
+
+                <div class="form-group">
+                    <label for="Usuario" class="col-lg-3 control-label">Nombre Usuario</label>
+                    <div class="col-lg-9">
+                        <input type="text" class="form-control" id="Usuario" placeholder="Nombre Usuario">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="Email" class="col-lg-3 control-label">E-mail</label>
+                    <div class="col-lg-9">
+                        <input type="email" class="form-control" id="Email" placeholder="Correo Electronico">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="Password" class="col-lg-3 control-label">Contraseña</label>
+                    <div class="col-lg-9">
+                        <input type="password" class="form-control" id="Password" placeholder="Contraseña">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="Password2" class="col-lg-3 control-label">Contraseña</label>
+                    <div class="col-lg-9">
+                        <input type="password" class="form-control" id="Password2" placeholder="Repita Contraseña">
+                    </div>
+                </div>
+              
+
+                <input type="submit" name="Aceptar">
+            </fieldset>
+        </form>
+
+    </body>
+</html>
