99ba0d0c36a75931b3600564069ef3cd6668fcf7
MaynorLop3z <maynorlop3z@gmail.com>
2015-08-20 23:07:56 -0600

    renombre el controlador PagPrincipal

--- application/controllers/PagPrincipal.php
+++ application/controllers/PagPrincipal.php
@@ -5,7 +5,7 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class pagPrincipal extends CI_Controller {
+class PagPrincipal extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
