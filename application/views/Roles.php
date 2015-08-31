c977856aec5b0a18296fa6b1f2a3dfe5f0dbe93b
johanna <rodrjoha1.3@gmail.com>
2015-08-30 21:53:54 -0600

    a peticion de ibarra

--- application/views/Roles.php
+++ application/views/Roles.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>Usuario Usuario</title>
+        <title>Roles Usuario</title>
 
         <!-- Bootstrap -->          
         <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -16,13 +16,16 @@
         <div class="container">
 
             <!-- Trigger the modal with a button,  Me falta centrar los botones el el div-->
-
+            <div class="row">
              <div class="btn btn-group">
-                <button href="#usuarioNuevo"  class="btn btn-default btn-default" data-toggle="modal">Asignar Rol</button>
-                <button href="#usuarioModifica" class="btn btn-default btn-default" data-toggle="modal">Modificar Rol</button>
-                <button href="#eliminarUsuario" class="btn btn-default btn-default" data-toggle="modal">Quitar Rol</button>
+                <button href="#modificarRol" class="btn btn-default btn-default" data-toggle="modal">Modificar Rol</button>
+                <!--    -->
+                <button href="#eliminarRol" class="btn btn-default btn-default" data-toggle="modal">Quitar Rol</button>
+            
+             </div>
             </div>
             <!-- DIv para la tabla  donde se muestran todos los usuario-->
+            <div class="row">
             <div class="col-lg-9">
                 <table class="table table-bordered table-striped table-hover table-responsive">
                     <thead>
@@ -39,6 +42,56 @@
                 </table>   
             </div>
             <div class="col-lg-3"></div>
-        </div> 
+        </div>
+        </div>
+        
+<!------Modal para el boton Asignar Rol----------------------------------------------------------------------------------->
+          <div id="modificarRol" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal-dialog">
+<div class="modal-content">
+	<div class="container-fluid ">
+        <form action="RolesController" class="form-horizontal" method="post" >
+            <fieldset>
+                <legend class="modal-header">Asignar Rol:</legend> 
+                <div class="form-group">
+                    <label for="selectUsuario" class="col-lg-3 control-label">Usuarios</label>
+                    <div class="col-lg-9">
+                        
+                        <select class="form-control" id="selectRol">
+                            <option>usuario 1</option>
+                            <option>Usuario 2</option>
+                            <option>Usuario 3</option>
+                          </select>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="selectRol" class="col-lg-3 control-label">Rol</label>
+                    <div class="col-lg-9">
+                        <select class="form-control" id="selectRol">
+                            <option>Secretaria Ingles</option>
+                            <option>Horas sociales</option>
+                            <option>Coordinador</option>
+                            <option>Docente</option>
+                            <option>Secretaria Diplomados</option>
+                            <option>Coordinadora de Ingles</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" id="btnEnviarR" onclick="" class=" btn btn-default" name="Aceptar">Aceptar</button>
+                    <button type="submit" id="btnCancelarR" onclick="" class=" btn btn-default" name="Limpiar">Cancelar</button>
+                </div>
+                </div>
+            </fieldset>
+        </form>
+</div>
+</div>
+</div>
+
+<!-- Quitar Rol-------------------------------------------------------------------------------------------->
+            
+        
+        
+        
     </body>
 </html>
