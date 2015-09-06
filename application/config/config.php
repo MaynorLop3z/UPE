3e22a6330c1fed8c7bebab6c7e95a43603abb9b3
Grisselda Santos <grissita13@gmail.com>
2015-09-05 23:13:21 -0600

    boton buscar

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/~maynorlop3z/UPE/';
+$config['base_url'] = 'http://localhost/UPE/';
 
 /*
 |--------------------------------------------------------------------------
