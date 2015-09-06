b6df166c075903ba3b413dd5eb72db0dee8b540a
root <root@luis-Latitude-D610>
2015-09-05 20:58:48 -0600

    lusho comit 5 de septiembre

---
+++ application/models/dto/UsuariosDTO.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of UsuariosDTO
+ *
+ * @author root
+ */
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class UsuariosDTO {
+
+    private $nombre;
+
+    public function __construct() {
+        
+    }
+
+    public function getNombre() {
+        return 'PruebaGEtySet';
+    }
+
+}
