0c3236d059165cb329f89d77a09c25611467bf19
johanna <rodrjoha1.3@gmail.com>
2015-09-21 17:54:40 -0600

    la parte de gestion de publicaciones

--- application/views/PublicacionesTab.php
+++ application/views/PublicacionesTab.php
@@ -1,8 +1,13 @@
+
 <div class="panel panel-default">
     <div class="panel-heading">
         <h3 class="panel-title">Gestion de Publicaciones</h3>
     </div>
     <div class="panel-body">
-
+        <div class="btn btn-group">
+            <button href="#NuevaPublicacion"  class="btn btn-default btn-default" data-toggle="modal">Nueva Publicacion</button>
+            <button href="#ModificarPublicacion" class="btn btn-default btn-default" data-toggle="modal">Modificar Publicacion</button>
+            <button href="#EliminarPublicacion" class="btn btn-default btn-default" data-toggle="modal">Eliminar Publicacion</button>
+        </div>
     </div>
 </div>
