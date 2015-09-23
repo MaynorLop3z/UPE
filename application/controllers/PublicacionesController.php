aaff5e18c9a764d5a4e5bf898f9ff5ae9a58ca6d
Maynor Lopez <maynorlop3z@gmail.com>
2015-09-23 15:39:10 -0600

    Error en la llamada de la vista

--- application/controllers/PublicacionesController.php
+++ application/controllers/PublicacionesController.php
@@ -13,7 +13,7 @@ class PublicacionesController extends CI_Controller {
     public function index() {
  
         
-        $this->load->view('Publicaiones');
+        $this->load->view('Publicaciones');
     }
 
 }
