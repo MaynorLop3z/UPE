4fe2ca01cd883edbfcbd6acebf0da0267549aa80
Grisselda Santos <grissita13@gmail.com>
2015-09-26 21:18:34 -0600

    modificaciones en el login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,23 +1,18 @@
-<?php
 
+<?php
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
-
 //controlador de la pagina principal, permite logear a los usuarios
 include './application/models/dto/UsuariosDTO.php';
 include './application/controllers/Listar.php';
-
 class Login extends CI_Controller {
-
     public $usuarioDTO;
     public $test;
-
     public function __construct() {
         parent::__construct();
         $this->usuarioDTO = new UsuariosDTO();
         $this->test = new Listar();
     }
-
     public function index() {
         $nombreUser = $this->usuarioDTO->getNombre();
         $test = $this->test->prueba();
@@ -25,12 +20,9 @@ class Login extends CI_Controller {
         $this->load->model('archivos');
         $user = $this->input->post('user');
         if ($user) {
-
             $this->load->model('usuario_model');
             $this->load->helper('url');
-
             if ($this->usuario_model->login($_POST['user'], $_POST['password'])) {
-
 //            if (true)   {
 //                
 //                $this->load->view('usuario');
@@ -53,7 +45,6 @@ class Login extends CI_Controller {
             $this->load->view('login_vista');
         }
     }
-
     private function colocarImg($ruta) {
         try {
             
@@ -61,5 +52,4 @@ class Login extends CI_Controller {
             $exc->getTrace();
         }
     }
-
 }
