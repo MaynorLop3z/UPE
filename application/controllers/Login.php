37b3b01d14f96ae0490560500775001ec7d2c030
Grisselda Santos <grissita13@gmail.com>
2015-10-11 14:15:13 -0600

    Carrusel desde  postgresql

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,21 +1,24 @@
 
 <?php
+
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 //controlador de la pagina principal, permite logear a los usuarios
 include './application/models/dto/UsuariosDTO.php';
 include './application/controllers/Listar.php';
+
 class Login extends CI_Controller {
+
     public $usuarioDTO;
-    public $test;
+
     public function __construct() {
         parent::__construct();
         $this->usuarioDTO = new UsuariosDTO();
         $this->test = new Listar();
     }
+
     public function index() {
         $nombreUser = $this->usuarioDTO->getNombre();
-        $test = $this->test->prueba();
         $this->load->model('publicaciones');
         $this->load->model('archivos');
         $user = $this->input->post('user');
@@ -23,34 +26,40 @@ class Login extends CI_Controller {
             $this->load->model('usuario_model');
             $this->load->helper('url');
             if ($this->usuario_model->login($_POST['user'], $_POST['password'])) {
-//            if (true)   {
-//                
-//                $this->load->view('usuario');
-                //$this->load->view('Dashboard'); 
+
                 redirect('Dashboard');
             } else {
-                //$this->load->view('login_vista');
-                echo 'jajajaj';
+                
             }
         } else {
-            //listar publicaciones con sus archivos
-            $publicacion = array();
-            $publicaciones = $this->publicaciones->listarPublicaciones();
-            $archivosList = array();
-            foreach ($publicaciones as $publicacion) {
-                //$publicacion->
-                $codigoPublicacion = $publicacion->CodigoPublicacion;
-                $archivosList = $this->archivos->listarArchivosPublicacion($codigoPublicacion);
-            }
-            $datosPublicacion = array();
-            $this->load->view('login_vista');
+            //aqui dene llamar usuarios
+
+            $data['publicacionesMostrar'] = $this->listarPublicaciones();
+            $this->load->view('login_vista', $data);
         }
     }
-    private function colocarImg($ruta) {
+
+    public function listarPublicaciones() {
         try {
-            
-        } catch (Exception $exc) {
-            $exc->getTrace();
+            $listaPublicacionesArchivos = array();
+            $iterador = 0;
+            $listaPublicaciones = array();
+            $listaPublicaciones = $this->publicaciones->listarPublicaciones();
+            foreach ($listaPublicaciones as $publicacion) {
+
+                $archivos = $this->archivos->listarArchivosPublicacion($publicacion->CodigoPublicacion);
+                foreach ($archivos as $archivo) {
+                    $publicacionArchivo = array('Titulo' => $publicacion->Titulo,
+                        'Ruta' => $archivo->Ruta);
+                }
+                array_push($listaPublicacionesArchivos, $publicacionArchivo);
+                $iterador ++;
+            }
+        } catch (Exception $ex) {
+            echo $ex->getTraceAsString();
         }
+
+        return $listaPublicacionesArchivos;
     }
+
 }
