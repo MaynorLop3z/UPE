97e1ed880c2cbf56edb813e59f1b4a086b7298a3
Grisselda Santos <grissita13@gmail.com>
2015-11-01 16:02:22 -0600

    crud de pulicaciones ...en proceso

--- bootstrap/js/Publicaciones.js
+++ bootstrap/js/Publicaciones.js
@@ -12,21 +12,21 @@ $(document).ready(function () {
 
 //funcion para guardar la img en la carpeta
     $("#divPub").submit(function (event)
-    
+
     {
         console.log("entro a la funcion");
         event.preventDefault();
         var $div = $(this);
         Titulo = $div.find("input[name='tituloPub']").val();
         console.log(Titulo),
-        Contenido = $div.children("textarea[name='cuerpoPub']").val();
+        Contenido = $div.find("textarea[name='contenido']").val();
         console.log(Contenido);
 //        url = base_url("index.php/PublicacionesController/do_upload");
-        url=".../application/controllers/PublicacionesController/do_upload";
-        console.log(url);
-        var posting = $.post(url, {titulo: Titulo, contenido: Contenido});
+        test = $div.find("input[name='escondido']").val();
+        console.log(test);
+        var posting = $.post(test, {titulo: Titulo, contenido: Contenido});
         console.log(posting);
-        
+
 
 
 
