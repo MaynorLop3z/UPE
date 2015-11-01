fe75be97ead556b2784febfad6198f2f3a0f74ac
Grisselda Santos <grissita13@gmail.com>
2015-11-01 10:46:53 -0600

    crud de pulicaciones ...en proceso

--- application/controllers/PublicacionesController.php
+++ application/controllers/PublicacionesController.php
@@ -8,12 +8,63 @@ class PublicacionesController extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
+        $this->load->helper(array('form', 'url'));
+        $this->load->model('publicaciones');
+        $this->load->model('archivos');
     }
 
     public function index() {
- 
-        
+
+
         $this->load->view('Publicaciones');
+    }
+
+    function do_upload() {
+        try {
+            $config['upload_path'] = './bootstrap/images/';
+            $config['allowed_types'] = 'jpg|png';
+            $config['max_size'] = '100';
+            $config['max_width'] = '1024';
+            $config['max_height'] = '768';
+
+            $this->load->library('upload', $config);
+            $dataImg = array();
+
+
+            if (!$this->upload->do_upload()) {
+                $error = array('error' => $this->upload->display_errors());
+                echo "error";
+                return $error;
+            } else {
+                if ($this->input->post()) {
+                    echo 'Entro al controller';
+                    //ingresar los datos de una publicacion a la bd
+                    $usuarioPublica = $this->session->userdata("codigoUserLogin");
+                    $fechaPublica = date('Y-m-d');
+                    $tituloP = $this->input->post('titulo');
+                    $contenidoP = $this->input->post('contenido');
+                    $arrayDataPublicacion = $this->publicaciones->CrearPublicacion($usuarioPublica, $fechaPublica, $tituloP, $contenidoP, TRUE, null, null, null, null, null);
+                    $codigoPub = $arrayDataPublicacion['CodigoPublicacion'];
+
+                    //ingresar los datos del archivo a la bd
+                    $dataImg = $this->upload->data();
+                    $ruta = "/images/" . $dataImg['file_name'];
+                    $nombre = $dataImg['file_name'];
+                    $extension = $dataImg['file_ext'];
+                    $estado = 'True';
+                    $codigoUsuario = $this->session->userdata("codigoUserLogin");
+                    $arrayDataArchivo = $this->CrearArchivo($ruta, $nombre, $extension, $estado, $codigoUsuario, $codigoPub);
+
+
+                    //echo json_encode();
+                }
+
+
+                return true;
+            }
+        } catch (Exception $exc) {
+            echo $exc->getTraceAsString();
+        }
     }
 
 }
