ba9ebb78d2f71ce882564cdbc021e27f308ea8b2
Grisselda Santos <grissita13@gmail.com>
2015-11-01 16:02:47 -0600

    crud de pulicaciones ...en proceso

--- application/views/Publicaciones/PublicacionesModal.php
+++ application/views/Publicaciones/PublicacionesModal.php
@@ -9,29 +9,31 @@
                 <button type="button" class="close btn-lg" data-dismiss="modal" aria-hidden="true">×</button>
                 <fieldset>
                     <legend class="modal-header"> Nueva Publicacion:</legend> 
-                    
+
 
                     <div class="form-group" class="form-inline" role="form" id="divPub">
                         <div class="form-group">
-                        <label for="DiplomadoNombre" class="col-lg-3 control-label">Titulo</label>
-                        <div class="col-lg-9">
-                            <input type="text" class="form-control"  placeholder="Titulo de la publicacion"  name="tituloPub">
+                            <label for="DiplomadoNombre" class="col-lg-3 control-label">Titulo</label>
+                            <div class="col-lg-9">
+                                <input type="text" class="form-control"  placeholder="Titulo de la publicacion"  name="tituloPub">
+                            </div>
                         </div>
-                    </div>
                         <label form="DiplomadoCuerpo" class="col-lg-3 control-label">Contenido</label>
                         <div class="col-lg-9">
                             <!--La linea de abajo abre el selector de imgs -->
-                            <!-- <form name="este" id="este" enctype="multipart/form-data" method="post" action="<?php //echo base_url() ?>index.php/PublicacionesController/do_upload">
+                            <!-- <form name="este" id="este" enctype="multipart/form-data" method="post" action="<?php //echo base_url()  ?>index.php/PublicacionesController/do_upload">
                             -->
                             <?php echo form_open_multipart('PublicacionesController/do_upload'); ?>   
                             <input type="file"  size="20" name="userfile" >
                             <br><br>
                             <input type="submit" value="upload"  id="uploadImg" name="upload">
-                            </form>
                             <br><br>
 
                             <!--en las lineas de abajo esta el cuerpo de la publicacion-->
-                            <textarea rows="5"  placeholder="Contenido" name=""></textarea>
+                            <textarea rows="5"  placeholder="Contenido" name="contenido" id=""></textarea>
+                            <input type="hidden" value="<?php echo base_url() ?>index.php/PublicacionesController/do_upload/" name="escondido">
+                            </form>
+
                         </div>
                     </div>
 
