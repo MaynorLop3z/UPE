57130cc94f38eb0295b34d06b81c43f04d7e5c8e
lichito <luisarrabi@gmail.com>
2016-03-20 20:09:33 -0600

    lusho comit  5del3del16

--- bootstrap/js/Roles.js
+++ bootstrap/js/Roles.js
@@ -1,4 +1,4 @@
-var codigoUsuario;
+var codigoR;
 $("#btnUsuarioNuevo").on('click', function () {
     $("#usuarioNuevo").modal();
 });
@@ -16,6 +16,8 @@ $('.btn_rls_user').on('click', function (event) {
     codigoUsuario = codigoUsuario.substring(6);
     $("#usuarioRoles").modal('show');
 });
+
+
 
 $('#usuarioModifica').on('show.bs.modal', function (event) {
     var tr = $('#tr' + codigoUsuario);
@@ -43,43 +45,15 @@ $('#usuarioRoles').on('show.bs.modal', function (event) {
 });
 
 
-$("#frmGuardarUSer").submit(function (event) {
+$("#frmGuardarR").submit(function (event) {
     event.preventDefault();
-    var $form = $(this), UsuarioNombre = $form.find("input[name='UsuarioNombre']").val(),
-            UsuarioPassword = $form.find("input[name='UsuarioPassword']").val(),
-            UsuarioEmail = $form.find("input[name='UsuarioEmail']").val(),
-            UsuarioNombreReal = $form.find("input[name='UsuarioNombreReal']").val(),
-            Comentarios = $form.find("textarea[name='Comentarios']").val(),
-            url = $form.attr("action");
-    var posting = $.post(url, {UsuarioNombre: UsuarioNombre,
-        UsuarioPassword: UsuarioPassword,
-        UsuarioEmail: UsuarioEmail,
-        Comentarios: Comentarios,
-        UsuarioNombreReal: UsuarioNombreReal});
+    var $form = $(this),RolNombre = $form.find("input[name='RolName']").val(),url = $form.attr("action");
+    var posting = $.post(url, {RolNombre: RolNombre});
     posting.done(function (data) {
         if (data !== null) {
-            var obj = jQuery.parseJSON(data);
-            var fila = '<tr id="tr' + obj.CodigoUsuario + '">';
-            fila = fila + '<td class="nombre_Usuario" >' + obj.Nombre + '</td>';
-            fila = fila + '<td class="correo_Usuario" >' + obj.CorreoUsuario + '</td>';
-            fila = fila + '<td class="nickName_Usuario" >' + obj.NombreUsuario + '</td>';
-            fila = fila + '<td style="text-align:center"  class="gestion_User">';
-            fila = fila + '</td></tr>';
-            $('#tableUsers > tbody').append(fila);
-            var trUser = $('#tableUsers > tbody').find("#tr" + obj.CodigoUsuario);
-            trUser.data("userd", obj);
-            var tdGestionUser = trUser.find(".gestion_User");
-            var divgestionUserBtn = $("#gestionUserBtn");
-
-            if (divgestionUserBtn !== null) {
-//                var divgestionUserBtnClone = divgestionUserBtn.clone(true);
-                alert('Crea el div');
-                var divgestionUserBtnClone = divgestionUserBtn;
-                divgestionUserBtnClone.find(".btn_modificar_user").attr("id", "" + obj.CodigoUsuario);
-                divgestionUserBtnClone.find(".btn_eliminar_user").attr("id", "btnDel" + obj.CodigoUsuario);
-                tdGestionUser.html(divgestionUserBtnClone);
-            }
-            $("#usuarioNuevo").modal('toggle');
+            $("#tableRol > tbody > tr").remove();
+            $('#tableRol > tbody').append(data);
+            
         }
     });
     posting.fail(function (data) {
